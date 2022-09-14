eeff90bdafb61e7ab22442668bd8d3b636ec14a8
umarmansoor001 <i190594@nu.edu.pk>
2022-09-15 00:13:46 +0500

    3 screens almost done

---
+++ app/src/main/java/com/umarmansoor/i190594/signin_withaccount.java
@@ -0,0 +1,14 @@
+package com.umarmansoor.i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signin_withaccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_withaccount);
+    }
+}
