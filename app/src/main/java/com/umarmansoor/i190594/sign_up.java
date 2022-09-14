949f564a99ee085db5c00b048f3b10acb57da0eb
umarmansoor001 <i190594@nu.edu.pk>
2022-09-14 11:25:49 +0500

    Initial commit

---
+++ app/src/main/java/com/umarmansoor/i190594/sign_up.java
@@ -0,0 +1,14 @@
+package com.umarmansoor.i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
