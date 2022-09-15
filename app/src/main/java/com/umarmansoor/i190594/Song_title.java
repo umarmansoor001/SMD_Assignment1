c5a61cd707b7f1c0b2c84a31bf3045cf0d1d6df0
umarmansoor001 <i190594@nu.edu.pk>
2022-09-15 20:12:24 +0500

    6 screens done

--- app/src/main/java/com/umarmansoor/i190594/Song_title.java
+++ app/src/main/java/com/umarmansoor/i190594/Song_title.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Song_title extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_music);
+        setContentView(R.layout.activity_song_title);
     }
 }
