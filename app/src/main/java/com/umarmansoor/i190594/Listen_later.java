bec54db5853a388e90e4df016b0bba73444f27f0
umarmansoor001 <i190594@nu.edu.pk>
2022-09-18 01:00:53 +0500

    17 screens done 18th need more changes

---
+++ app/src/main/java/com/umarmansoor/i190594/Listen_later.java
@@ -0,0 +1,14 @@
+package com.umarmansoor.i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listen_later extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_later);
+    }
+}
