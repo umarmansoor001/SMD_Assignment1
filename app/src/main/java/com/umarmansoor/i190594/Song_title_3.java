0d8c2166623f63fba8bdbfc0a3bbbd79ec9ce8da
umarmansoor001 <i190594@nu.edu.pk>
2022-09-17 16:08:56 +0500

    14 screens done

---
+++ app/src/main/java/com/umarmansoor/i190594/Song_title_3.java
@@ -0,0 +1,14 @@
+package com.umarmansoor.i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Song_title_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_title3);
+    }
+}
