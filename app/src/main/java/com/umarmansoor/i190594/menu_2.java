3754c73a69ad5d56bf20f6072cf288b8b92dbc00
umarmansoor001 <i190594@nu.edu.pk>
2022-09-18 17:40:46 +0500

    19 screens done & 20th,21th,22th need more changes

---
+++ app/src/main/java/com/umarmansoor/i190594/menu_2.java
@@ -0,0 +1,14 @@
+package com.umarmansoor.i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
